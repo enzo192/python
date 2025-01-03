f58df4ea250d88dff76d38ee6118c5bf0869ba8c
Enzo <enzo192@gmail.com>
2025-01-02 20:40:55 -0500

    diccionarios

--- clase10_diccionario.py
+++ clase10_diccionario.py
@@ -21,3 +21,26 @@ contacts = {"Carla": {"Apellido": "Florida",
                "Altura": 1.80,
                "Edad": 32}}
 print(contacts["Carla"])
+
+#comprehension list
+#una forma mas facil de hacer diccionarios
+
+squares = [x**2 for x in range(1,11)]
+#print("Cuadrados:", squares)
+
+celsius = [0, 10, 20, 30, 40]
+fahrenheit = [((9/5)*temp + 32) for temp in celsius]
+#print("Temperatura en F: ", fahrenheit)
+
+#Numeros pares
+evens = [x for x in range(1,21) if x%2 == 0]
+#print("Pares: ", evens)
+
+matrix = [[1,2,3], 
+          [4,5,6], 
+          [7,8,9]]
+
+transposed = [[row[i] for row in matrix] for i in range(3)]
+print(matrix)
+print(transposed)
+
